15fa77f06194f63cc3fbd324f99fd5d873202279
karucida <carlosalbertomix10@gmail.com>
2017-03-07 22:21:37 +0100

    Telegram en python y php

---
+++ php.php
@@ -0,0 +1,13 @@
+<?php
+$token="XXXXX";
+$url='https://api.telegram.org/bot'.$token.'/');
+
+$request=file_get_contents($url.'getUpdates');
+foreach($request['result'] as $message){
+	$chat_id=$message['message']['chat']['id']
+
+	if($message['message']['text']=='/start'){
+		$text=urlencode('Hello World');
+		echo file_get_contents($url.'sendMessage?chat_id='.$chat_id.'&text='.$text)
+	}
+} ?>
---
+++ python.py
@@ -0,0 +1,13 @@
+import requests
+from pprint import pprint
+url = "https://api.telegram.org/botXXXXX/{}"
+r = requests.get(url.format('getUpdates')).json()
+r = r['result']
+
+for message in r:
+  chat_id = message['message']['chat']['id']
+  if message['message'].get('text') and message['message'].get('text') == '/start':
+    params = {'text':'Hello World', 'chat_id':chat_id}
+    r2 = requests.get(url.format('sendMessage'), params)
+    pprint(r2.json())
+
