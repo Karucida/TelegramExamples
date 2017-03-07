bb53afd8951234e55b93b7d7b25ec0df1c09c752
karucida <carlosalbertomix10@gmail.com>
2017-03-07 22:34:21 +0100

    php error

--- php.php
+++ php.php
@@ -1,13 +1,14 @@
 <?php
 $token="XXXXX";
-$url='https://api.telegram.org/bot'.$token.'/');
+$url='https://api.telegram.org/bot'.$token.'/';
 
-$request=file_get_contents($url.'getUpdates');
+$request=json_decode(file_get_contents($url.'getUpdates'),true);
+
 foreach($request['result'] as $message){
-	$chat_id=$message['message']['chat']['id']
+	$chat_id=$message['message']['chat']['id'];
 
 	if($message['message']['text']=='/start'){
 		$text=urlencode('Hello World');
-		echo file_get_contents($url.'sendMessage?chat_id='.$chat_id.'&text='.$text)
+		echo file_get_contents($url.'sendMessage?chat_id='.$chat_id.'&text='.$text);
 	}
 } ?>
